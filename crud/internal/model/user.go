3ccd2e9fcd71eae0da0bbe4c831375ca4876cd31
rahulchaurasiya2981-droid <rahul.chaurasiya2981@gmail.com>
2026-09-13 11:47:39 +0530

    feat: add user model

---
+++ crud/internal/model/user.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Age       int       `json:"age"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
